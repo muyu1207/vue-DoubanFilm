2c747e711d1c2812087bf7fe2f4509c321eeb1b4
zhangxu <1306606222@qq.com>
2017-07-31 10:03:22 +0800

    豆瓣电影接口数据已经好了

--- src/views/douban/index.vue
+++ src/views/douban/index.vue
@@ -1,39 +1,47 @@
 <template>
   <div id="demssço">
 
-    <!-- <el-card class="box-card">
+    <el-card class="box-card">
       <div slot="header" class="clearfix">
         <h1 class="middle" style="line-height: 36px; color: #20A0FF">{{msg}}</h1>
       </div>
-      <div v-for="article in articles" class="text item">
-        
-        <div><img :src="article.images.small" /></div>
-        <div>{{article.title}}</div>
-      </div>
-    </el-card> -->
 
-    <el-table :data="articles"  style="width: 100%;margin:0px auto;">
-      
-      <el-table-column    label="ID"  width="180">
+      <el-table :data="articles"  style="width: 100%;margin:0px auto;">
+
+        <el-table-column    label="ID"  width="100">
           <template scope="scope">
             {{scope.row.id}}
           </template>
-      </el-table-column>
+        </el-table-column>
 
-      <el-table-column  label="标题" width="180">
-           <template scope="scope">
-                {{scope.row.title}}
-          </template>
-      </el-table-column>
-
-      <el-table-column  label="封面" >
+        <el-table-column  label="标题" width="120">
           <template scope="scope">
-               <img class="filmcover" :src="scope.row.images.small" />
+            {{scope.row.title}}
           </template>
-          
-      </el-table-column>
+        </el-table-column>
 
-    </el-table>
+        <el-table-column  label="评分" width="80">
+          <template scope="scope">
+            {{scope.row.rating.average}}
+          </template>
+        </el-table-column>
+
+        <el-table-column  label="年份" width="80">
+          <template scope="scope">
+            {{scope.row.year}}
+          </template>
+        </el-table-column>
+
+        <el-table-column  label="封面" >
+          <template scope="scope">
+            <img class="filmcover" :src="scope.row.images.small" />
+          </template>
+        </el-table-column>
+
+      </el-table>
+    </el-card>
+
+
 
 
   </div>
@@ -52,11 +60,11 @@
     },
     components: { DemoComm },
     mounted: function() {
-        this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=40', {}, {
-        headers: {
-
-        },
-        emulateJSON: true
+        this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
+//        headers: {
+//
+//        },
+//        emulateJSON: true
     }).then(function(response){
         this.articles = response.data["subjects"]
           console.log(this.articles)
@@ -73,7 +81,7 @@
   .filmcover{
     width:30px;
   }
-  
+
 
 </style>
 
